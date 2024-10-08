f98d7190b2569ca317ca951afb8128010b301783
michaelgroves90 <67477479+michaelgroves90@users.noreply.github.com>
2024-10-08 16:32:10 +0100

    CountriesServiceSpec updated

--- test/uk/gov/hmrc/cardpaymentfrontend/services/CountriesServiceSpec.scala
+++ test/uk/gov/hmrc/cardpaymentfrontend/services/CountriesServiceSpec.scala
@@ -1,6 +1,17 @@
 /*
  * Copyright 2024 HM Revenue & Customs
  *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 package uk.gov.hmrc.cardpaymentfrontend.services
@@ -27,33 +38,33 @@ class CountriesServiceSpec extends AnyWordSpec with Matchers with ScalaFutures w
     "fetch country codes from government register for English" in {
       val countries = service.getCountries(fakeGetRequest())
       countries.size shouldBe 249
-      countries.head.code shouldBe "GBR"
-      countries(1).code shouldBe "AFG"
-      countries.last.code shouldBe "ZWE"
+      countries.headOption.map(_.code) shouldBe Some("GBR")
+      countries.drop(1).headOption.map(_.code) shouldBe Some("AFG")
+      countries.lastOption.map(_.code) shouldBe Some("ZWE")
     }
 
     "fetch country codes from government register for Welsh" in {
       val countries = service.getCountries(fakeGetRequestInWelsh())
       countries.size shouldBe 249
-      countries.head.code shouldBe "GBR"
-      countries(1).code shouldBe "AFG"
-      countries.last.code shouldBe "GRL"
+      countries.headOption.map(_.code) shouldBe Some("GBR")
+      countries.drop(1).headOption.map(_.code) shouldBe Some("AFG")
+      countries.lastOption.map(_.code) shouldBe Some("GRL")
     }
 
     "fetch country names from government register for English" in {
       val countries = service.getCountries(fakeGetRequest())
       countries.size shouldBe 249
-      countries.head.name shouldBe "United Kingdom"
-      countries(1).name shouldBe "Afghanistan"
-      countries.last.name shouldBe "Zimbabwe"
+      countries.headOption.map(_.name) shouldBe Some("United Kingdom")
+      countries.drop(1).headOption.map(_.name) shouldBe Some("Afghanistan")
+      countries.lastOption.map(_.name) shouldBe Some("Zimbabwe")
     }
 
     "fetch country names from government register for Welsh" in {
       val countries = service.getCountries(fakeGetRequestInWelsh())
       countries.size shouldBe 249
-      countries.head.name shouldBe "Y Deyrnas Unedig"
-      countries(1).name shouldBe "Affganistan"
-      countries.last.name shouldBe "Yr Ynys Las"
+      countries.headOption.map(_.name) shouldBe Some("Y Deyrnas Unedig")
+      countries.drop(1).headOption.map(_.name) shouldBe Some("Affganistan")
+      countries.lastOption.map(_.name) shouldBe Some("Yr Ynys Las")
     }
 
   }
