affe50f0daec454663a45e9593da982d5da85078
michaelgroves90 <67477479+michaelgroves90@users.noreply.github.com>
2025-05-12 09:20:37 +0100

    WIP

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -15,7 +15,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq[ModuleID](
     "uk.gov.hmrc"  %% "bootstrap-frontend-play-30"       % bootstrapVersion,
-    "uk.gov.hmrc"  %% "play-frontend-hmrc-play-30"       % "12.0.0",
+    "uk.gov.hmrc"  %% "play-frontend-hmrc-play-30"       % "12.1.0",
     "com.beachape" %% "enumeratum"                       % "1.7.6",
     "com.beachape" %% "enumeratum-play"                  % "1.8.2",
     "uk.gov.hmrc"  %% "pay-api-cor-card-payment-journey" % payApiCorVersion excludeAll(payApiExclusionRules: _*)
