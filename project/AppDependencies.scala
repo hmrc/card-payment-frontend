ee772d95222e9f7f077e9b2f0ec97afab7f69c57
tp10 <1626193+tp10@users.noreply.github.com>
2025-07-22 17:59:01 +0100

    [TP][OPS-13880] - Swap H4 to H2 to give sequentional header priorities

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt.*
 object AppDependencies {
 
   val payApiCorVersion = "1.259.0"
-  private val bootstrapVersion = "9.13.0"
+  private val bootstrapVersion = "9.18.0"
 
   private val payApiExclusionRules: Seq[InclusionRule] = Seq(
     ExclusionRule(organization = "uk.gov.hmrc", name = "bootstrap-backend-play-29_2.13"),
@@ -15,7 +15,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq[ModuleID](
     "uk.gov.hmrc"  %% "bootstrap-frontend-play-30"       % bootstrapVersion,
-    "uk.gov.hmrc"  %% "play-frontend-hmrc-play-30"       % "12.5.0",
+    "uk.gov.hmrc"  %% "play-frontend-hmrc-play-30"       % "12.7.0",
     "com.beachape" %% "enumeratum"                       % "1.9.0",
     "com.beachape" %% "enumeratum-play"                  % "1.9.0",
     "uk.gov.hmrc"  %% "pay-api-cor-card-payment-journey" % payApiCorVersion excludeAll(payApiExclusionRules *)
@@ -23,7 +23,7 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = Seq[ModuleID](
     "uk.gov.hmrc" %% "bootstrap-test-play-30" % bootstrapVersion,
-    "org.jsoup"   %  "jsoup"                  % "1.20.1"
+    "org.jsoup"   %  "jsoup"                  % "1.21.1"
   ).map( _ % Test )
 
   val it: Seq[ModuleID] = Seq.empty[ModuleID]
