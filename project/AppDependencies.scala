a18e8c0152a74f0a5ad37c46ff25717a1fc7c767
Jake Barnes <33868990+barnesjake@users.noreply.github.com>
2025-08-05 17:19:45 +0100

    [JB][OPS-13720] hide styling of redirect to parent page within iframe (#87)
    * [JB][OPS-13720] hide styling of redirect to parent page within iframe
    
    * rework

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,8 +3,9 @@ import sbt.*
 //format: OFF
 object AppDependencies {
 
+
   private val payApiCorVersion = "1.261.0"
-  private val bootstrapVersion = "9.19.0"
+  private val bootstrapVersion = "10.0.0"
 
   private val payApiExclusionRules: Seq[InclusionRule] = Seq(
     ExclusionRule(organization = "uk.gov.hmrc", name = "bootstrap-backend-play-29_2.13"),
