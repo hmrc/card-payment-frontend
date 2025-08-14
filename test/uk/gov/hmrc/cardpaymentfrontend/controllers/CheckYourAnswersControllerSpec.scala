0e46900b7dd511f1b9549c46133f0bd6b95233d9
michaelgroves90 <67477479+michaelgroves90@users.noreply.github.com>
2025-08-14 10:44:10 +0100

    What happens content fix for VatC2c

--- test/uk/gov/hmrc/cardpaymentfrontend/controllers/CheckYourAnswersControllerSpec.scala
+++ test/uk/gov/hmrc/cardpaymentfrontend/controllers/CheckYourAnswersControllerSpec.scala
@@ -223,10 +223,10 @@ class CheckYourAnswersControllerSpec extends ItSpec {
       val tdJourney: Journey[JourneySpecificData] = TestHelpers.deriveTestDataFromOrigin(origin).journeyBeforeBeginWebPayment
 
       val shouldBeAbleToChangeAmount: Boolean = origin match {
-        case Origins.WcSa => false
-        case Origins.WcCt => false
+        case Origins.WcSa   => false
+        case Origins.WcCt   => false
         case Origins.VatC2c => false
-        case _ => true
+        case _              => true
       }
 
       s"[${origin.entryName}] should render the amount row correctly" in {
