82d9242f4bd9c6c3d3c87c68d64ee16ec311fa30
michaelgroves90 <67477479+michaelgroves90@users.noreply.github.com>
2025-11-14 11:17:30 +0000

    null removed

--- test/uk/gov/hmrc/cardpaymentfrontend/views/LayoutSpec.scala
+++ test/uk/gov/hmrc/cardpaymentfrontend/views/LayoutSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.cardpaymentfrontend.views
 
 import org.jsoup.Jsoup
@@ -63,7 +79,6 @@ class LayoutSpec extends ItSpec {
     val document = Jsoup.parse(contentAsString(result))
 
     val backLinkElement = document.select(".govuk-back-link").first()
-    backLinkElement should not be null
     backLinkElement.text().trim shouldBe "Back"
     backLinkElement.attr("href") shouldBe "#"
   }
@@ -76,7 +91,6 @@ class LayoutSpec extends ItSpec {
     val document = Jsoup.parse(contentAsString(result))
 
     val techIssueLink = document.select("a.hmrc-report-technical-issue").first()
-    techIssueLink should not be null
     techIssueLink.text().trim shouldBe "Is this page not working properly? (opens in new tab)"
     techIssueLink.attr("href") should include("/contact/report-technical-problem")
   }
