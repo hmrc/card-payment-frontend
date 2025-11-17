a13c25c63acbe9234c5f90c23f1ff7c6297d147a
raysonviswas1 <119664395+raysonviswas1@users.noreply.github.com>
2025-11-17 16:53:41 +0000

    [RV][OPS-14223] fix number of implemented origins

--- test/uk/gov/hmrc/cardpaymentfrontend/services/ClientIdServiceSpec.scala
+++ test/uk/gov/hmrc/cardpaymentfrontend/services/ClientIdServiceSpec.scala
@@ -218,7 +218,7 @@ class ClientIdServiceSpec extends ItSpec {
   }
 
   "sanity check for implemented origins" in {
-    TestHelpers.implementedOrigins.size shouldBe 71 withClue "** This dummy test is here to remind you to update the tests above. Bump up the expected number when an origin is added to implemented origins **"
+    TestHelpers.implementedOrigins.size shouldBe 70 withClue "** This dummy test is here to remind you to update the tests above. Bump up the expected number when an origin is added to implemented origins **"
   }
 
 }
