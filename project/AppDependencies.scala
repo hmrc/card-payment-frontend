292fe4d038e71fe55ba912df4f2d6ea70fff98cb
barnesjake <jake.barnes@digital.hmrc.gov.uk>
2025-12-08 16:28:27 +0000

    [JB][OPS-14287] bump up pay-api cor

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,7 +4,7 @@ import sbt.*
 object AppDependencies {
 
 
-  private val payApiCorVersion = "1.284.0-SNAPSHOT"
+  private val payApiCorVersion = "1.284.0"
   private val bootstrapVersion = "10.4.0"
 
   private val payApiExclusionRules: Seq[InclusionRule] = Seq(
